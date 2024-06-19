c1f6ae7842202e0b4bba424ffe2dec513dc66dcc
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-19 21:08:59 +0200

    ej 1

--- loops.js
+++ loops.js
@@ -1,10 +1,12 @@
 //Ej 1: 
-for (let i = 0; i <= 10; i++) {
-    console.log(i); 
-}
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i); 
+// }
 
-let i = 0
-while (i <= 10) {
-    console.log(i)
-    i++
-}
+// let i = 0
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+// Ej 2:
